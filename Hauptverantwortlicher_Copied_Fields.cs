a19eec0fc582756994a7e5d594bc2aa17859e77e
Acar Gökhan - BAUM Unternehmensgruppe <acar@baum-gruppe.com>
2020-07-16 23:25:49 +0200

    Add project files.

---
+++ Hauptverantwortlicher_Copied_Fields.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Update_Hauptverantwortliche
+{
+    public partial class test_Hauptverant
+    {
+        public test_Hauptverant(test_Hauptverant copyEntry)
+        {
+            this.STICHTAG = copyEntry.STICHTAG;
+            this.MANDANT = copyEntry.MANDANT;
+            this.OBJEKTGES = copyEntry.OBJEKTGES;
+            this.LIEGNR = copyEntry.LIEGNR;
+            this.LIEGBEZ = copyEntry.LIEGBEZ;
+            this.Objektschluessel_Liegenschaft = copyEntry.Objektschluessel_Liegenschaft;
+            this.Buchungskreis = copyEntry.Buchungskreis;
+            this.Hauptverantwortlich = copyEntry.Hauptverantwortlich;
+        }
+
+        public test_Hauptverant()
+        {
+
+        }
+
+        public void Set_COO_AL(test_Hauptverant firstItem)
+        {
+            firstItem.User_Kategorie = "COO - AL";
+            firstItem.User_Name_Vorname = "Siemens Harald";
+            firstItem.Zweit_Prüfer_Berechtigt = "AL";
+        }
+
+        public void Set_COO_GF(test_Hauptverant firstItem)
+        {
+            firstItem.User_Kategorie = "COO - GF";
+            firstItem.User_Name_Vorname = "Siemens Harald";
+            firstItem.Zweit_Prüfer_Berechtigt = "GF";
+        }
+
+        public void Set_CEO_AL(test_Hauptverant firstItem)
+        {
+            firstItem.User_Kategorie = "CEO - AL";
+            firstItem.User_Name_Vorname = "Baum Gregor";
+            firstItem.Zweit_Prüfer_Berechtigt = "AL";
+        }
+
+        public void Set_Leitung_Finanzen_AL(test_Hauptverant firstItem)
+        {
+            firstItem.User_Kategorie = "Leitung Finanzen - AL";
+            firstItem.User_Name_Vorname = "Acar Gökhan";
+            firstItem.Zweit_Prüfer_Berechtigt = "AL";
+        }
+        public void Set_Leitung_Mietbetreuung_AL(test_Hauptverant firstItem)
+        {
+            firstItem.User_Kategorie = "Leitung Mietbetreuung - AL";
+            firstItem.User_Name_Vorname = "Röske Melanie";
+            firstItem.Zweit_Prüfer_Berechtigt = "AL";
+        }
+        public void Set_Leitung_Projektsteuerung_AL(test_Hauptverant firstItem)
+        {
+            firstItem.User_Kategorie = "Leitung Projektsteuerung - AL";
+            firstItem.User_Name_Vorname = "Besser Fabian";
+            firstItem.Zweit_Prüfer_Berechtigt = "AL";
+        }
+        public void Set_Leitung_Rechnungswesen_AL(test_Hauptverant firstItem)
+        {
+            firstItem.User_Kategorie = "Leitung Rechnungswesen - AL";
+            firstItem.User_Name_Vorname = "Lange Holger";
+            firstItem.Zweit_Prüfer_Berechtigt = "AL";
+        }
+
+        public void Set_Leitung_Recht_AL(test_Hauptverant firstItem)
+        {
+            firstItem.User_Kategorie = "Leitung Recht - AL";
+            firstItem.User_Name_Vorname = "Stern Susanne";
+            firstItem.Zweit_Prüfer_Berechtigt = "AL";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Update_Hauptverantwortliche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(DOCUWARE_TABELLENEntities context = new DOCUWARE_TABELLENEntities())
+            {
+                List<double?> result = context.test_Hauptverant.Select(x => x.Objektschluessel_Liegenschaft).Distinct().ToList();
+                var GroupResult = context.test_Hauptverant.GroupBy(x => x.Objektschluessel_Liegenschaft).ToList();
+
+                //Console.WriteLine(string.Join(Environment.NewLine, result));
+
+                foreach (var item in GroupResult)
+                {
+                    test_Hauptverant firstItem = item.First();
+                    test_Hauptverant newEntry = new test_Hauptverant(firstItem);
+
+                    newEntry.Set_COO_AL(newEntry);
+                    context.test_Hauptverant.Add(newEntry);
+                    
+                    newEntry = new test_Hauptverant(firstItem);
+                    newEntry.Set_COO_GF(newEntry);
+                    context.test_Hauptverant.Add(newEntry);
+
+                    newEntry = new test_Hauptverant(firstItem);
+                    newEntry.Set_CEO_AL(newEntry);
+                    context.test_Hauptverant.Add(newEntry);
+
+                    newEntry = new test_Hauptverant(firstItem);
+                    newEntry.Set_Leitung_Finanzen_AL(newEntry);
+                    context.test_Hauptverant.Add(newEntry);
+
+                    newEntry = new test_Hauptverant(firstItem);
+                    newEntry.Set_Leitung_Mietbetreuung_AL(newEntry);
+                    context.test_Hauptverant.Add(newEntry);
+
+                    newEntry = new test_Hauptverant(firstItem);
+                    newEntry.Set_Leitung_Projektsteuerung_AL(newEntry);
+                    context.test_Hauptverant.Add(newEntry);
+
+                    newEntry = new test_Hauptverant(firstItem);
+                    newEntry.Set_Leitung_Rechnungswesen_AL(newEntry);
+                    context.test_Hauptverant.Add(newEntry);
+
+                    newEntry = new test_Hauptverant(firstItem);
+                    newEntry.Set_Leitung_Recht_AL(newEntry);
+                    context.test_Hauptverant.Add(newEntry);
+                }
+               
+                context.SaveChanges();
+
+                Console.ReadLine();
+
+            }
+
+        }
+    }
+}
